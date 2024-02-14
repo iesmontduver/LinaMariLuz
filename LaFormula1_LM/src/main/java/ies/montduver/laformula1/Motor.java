03aa3e1a04bae549e26ba97f8d5a6a2124ba820e
aderosgor <aderosgor@INF1-PC01>
2024-02-14 13:29:06 +0100

    He creat la classe motor, escuderia i pilot.

---
+++ LaFormula1_LM/src/main/java/ies/montduver/laformula1/Motor.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ies.montduver.laformula1;
+
+/**
+ *
+ * @author aderosgor
+ */
+public class Motor {
+    
+    private String tipo;
+    private double cilindrada;
+    private int potencia;
+
+    public Motor(String tipo, double Cilindrada, int potencia) {
+        this.tipo = tipo;
+        this.cilindrada = Cilindrada;
+        this.potencia = potencia;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public double getCilindrada() {
+        return cilindrada;
+    }
+
+    public int getPotencia() {
+        return potencia;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public void setCilindrada(double Cilindrada) {
+        this.cilindrada = Cilindrada;
+    }
+
+    public void setPotencia(int potencia) {
+        this.potencia = potencia;
+    }
+    
+    
+}
---
+++ LaFormula1_LM/src/main/java/ies/montduver/laformula1/Pilot.java
@@ -0,0 +1,68 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ies.montduver.laformula1;
+
+/**
+ *
+ * @author aderosgor
+ */
+public class Pilot {
+    
+    private String nom;
+    private Escuderia escuderia;
+    private int numero;
+    private int pilot;
+    private double sou;
+
+    public Pilot(String nom, Escuderia escuderia, int numero, int pilot, double sou) {
+        this.nom = nom;
+        this.escuderia = escuderia;
+        this.numero = numero;
+        this.pilot = pilot;
+        this.sou = sou;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public Escuderia getEscuderia() {
+        return escuderia;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public int getPilot() {
+        return pilot;
+    }
+
+    public double getSou() {
+        return sou;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public void setEscuderia(Escuderia escuderia) {
+        this.escuderia = escuderia;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public void setPilot(int pilot) {
+        this.pilot = pilot;
+    }
+
+    public void setSou(double sou) {
+        this.sou = sou;
+    }
+    
+    
+}
